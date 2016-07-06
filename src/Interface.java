e79d3f0a81c4d6c810c6eb84c314e77a853cda93
Tim <Tim@Tim-HP>
2016-07-06 14:19:24 +0200

    Removed some unnecessary code.

--- src/Interface.java
+++ src/Interface.java
@@ -1,55 +1,101 @@
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.Window;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-public class Interface extends JFrame implements ItemListener {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5529336786288984928L;
-	
-	private JPanel superPanel = new JPanel();
-	private JPanel cards = new JPanel(new CardLayout());
-	private final String ATTRIBUTE_PANEL = "Attributes";
+public class Interface implements ItemListener {
+    JPanel cards; //a panel that uses CardLayout
+    private final String ATTRIBUTE_PANEL = "Attributes";
 	private final String ANONYMIZATION_PANEL = "Anonymization";
-	
-	
-	public Interface() {
-		setTitle("ARX Interface Alpha");
-		setVisible(true);
-		addCards();
-		getContentPane().add(superPanel);
-		getContentPane().add(cards, BorderLayout.CENTER);
-		pack();
-		setSize(480, 240);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	private void addCards() {
-		JPanel comboBoxPane = new JPanel();
-		String[] items = {ATTRIBUTE_PANEL, ANONYMIZATION_PANEL};
-		JComboBox<String> cb = new JComboBox<String>(items);
-		cb.setEditable(false);
-		cb.addItemListener(this);
-		comboBoxPane.add(cb);
-		
-		JPanel card1 = new AttributePanel();
-		
-		cards.add(card1, ATTRIBUTE_PANEL);
-		
-		superPanel.add(comboBoxPane, BorderLayout.PAGE_START);
-		superPanel.add(cards, BorderLayout.CENTER);
-	}
-
-	@Override
-	public void itemStateChanged(ItemEvent arg0) {
-		CardLayout layout = (CardLayout) cards.getLayout();
-		layout.show(cards, (String) arg0.getItem());
-	}	
+     
+    public void addComponentToPane(Container pane) {
+        //Put the JComboBox in a JPanel to get a nicer look.
+        JPanel comboBoxPane = new JPanel(); //use FlowLayout
+        String comboBoxItems[] = { ATTRIBUTE_PANEL, ANONYMIZATION_PANEL };
+        JComboBox<String> cb = new JComboBox<String>(comboBoxItems);
+        cb.setEditable(false);
+        cb.addItemListener(this);
+        comboBoxPane.add(cb);
+         
+        //Create the "cards".
+        JPanel card1 = new AttributePanel();
+         
+        JPanel card2 = new AnonymizationPanel();
+         
+        //Create the panel that contains the "cards".
+        cards = new JPanel(new CardLayout());
+        cards.add(card1, ATTRIBUTE_PANEL);
+        cards.add(card2, ANONYMIZATION_PANEL);
+         
+        pane.add(comboBoxPane, BorderLayout.PAGE_START);
+        pane.add(cards, BorderLayout.CENTER);
+    }
+     
+    public void itemStateChanged(ItemEvent evt) {
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, (String)evt.getItem());
+    }
+     
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("ARX Interface Alpha");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+        //Create and set up the content pane.
+        CardLayoutDemo demo = new CardLayoutDemo();
+        demo.addComponentToPane(frame.getContentPane());
+         
+        //Display the window.
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+        centreWindow(frame);
+    }
+     
+    public static void main(String[] args) {
+        /* Use an appropriate Look and Feel */
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", false);
+         
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    
+    public static void centreWindow(Window frame) {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+        frame.setLocation(x, y);
+    }
 }

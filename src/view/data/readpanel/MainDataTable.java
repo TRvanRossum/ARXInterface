e0a69961b7b9e4a602908f6aa5d141acd7cba557
Tim <Tim@145.94.216.25>
2016-08-02 17:40:37 +0200

    Committing before changing a major part of the interface....

---
+++ src/view/data/readpanel/MainDataTable.java
@@ -0,0 +1,43 @@
+package view.data.readpanel;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import io.Data;
+
+public class MainDataTable extends JPanel{
+	
+	private String[] atts;
+	private String[][] data;
+	private JTable dataTable;
+	
+	public MainDataTable(Data d) {
+		atts = d.getAttributes();
+		data = d.getData();
+		
+		dataTable = new JTable(data, atts){
+            /**
+			 * 
+			 */
+			private static final long serialVersionUID = -3803735335044275361L;
+
+			public boolean getScrollableTracksViewportWidth()
+            {
+                return getPreferredSize().width < getParent().getWidth();
+            }
+        };
+		
+		dataTable.setRowSelectionAllowed( true );
+		dataTable.setColumnSelectionAllowed( true );
+
+		dataTable.setSelectionForeground( Color.white );
+		dataTable.setSelectionBackground( Color.red );
+		
+		dataTable.setEnabled(false);
+		
+		new JScrollPane(dataTable);
+	}
+}

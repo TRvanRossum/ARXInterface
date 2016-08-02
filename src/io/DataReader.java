885c466bd92311c11a812f23b2fcc8be7c12b912
Tim <Tim@145.94.216.25>
2016-08-02 14:24:59 +0200

    Worked on the data reading a bit.

--- src/io/DataReader.java
+++ src/io/DataReader.java
@@ -1,4 +1,11 @@
 package io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+
 /**
  * Singleton DataReader class, in order to read data from files.
  * @author Tim
@@ -14,5 +21,24 @@ public class DataReader {
 	
 	public static DataReader getReader(){
 		return dr;
+	}
+	
+	public Data readData(String filepath, String delimiter) throws IOException{
+		LineNumberReader lnr = new LineNumberReader(new FileReader(new File(filepath)));
+		BufferedReader br = new BufferedReader(new FileReader(new File(filepath)));
+		lnr.skip(Long.MAX_VALUE);
+		int rows = lnr.getLineNumber();
+		lnr.close();
+		
+		String[] columnData = br.readLine().split(delimiter);
+		String[][] allData = new String[rows][columnData.length];
+		for(int i = 0; i < rows; i++){
+			allData[i] = br.readLine().split(delimiter);
+		}
+		
+		br.close();
+		
+		Data res = new Data(columnData, allData);
+		return res;
 	}
 }

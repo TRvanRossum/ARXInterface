fffd291e19ae784e991ed38931bd1cb3127e2b1e
Tim <Tim@145.94.216.25>
2016-08-02 15:48:05 +0200

    Working on choosing a file to read data from.

--- src/DataTable.java
+++ src/DataTable.java
@@ -1,6 +1,11 @@
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -14,6 +19,9 @@ public class DataTable extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = -1302255224765149924L;
+	
+	private String selectedFile = "No file selected.";
+	private JLabel selectedFileLabel = new JLabel(selectedFile);
 	
 	public DataTable(Data d) {
 		setLayout(new GridLayout(2, 1));
@@ -42,6 +50,35 @@ public class DataTable extends JPanel {
 		
 		JScrollPane pane = new JScrollPane(table);
 		add(pane);
-		add(new JLabel("Data reading coming soon..."));
+		add(createDataReaderTable());
+	}
+	
+	private JPanel createDataReaderTable(){
+		JPanel res = new JPanel();
+		
+		res.setLayout(new GridLayout(2, 2));
+		
+		JButton fileChooseButton = new JButton("Choose a file...");
+		res.add(fileChooseButton);
+		fileChooseButton.addActionListener(new ActionListener() {
+			//Handle open button action.
+		    public void actionPerformed(ActionEvent e) {
+		        final JFileChooser fc = new JFileChooser(); 
+		        int returnVal = fc.showOpenDialog(res);
+		        if (returnVal == JFileChooser.APPROVE_OPTION) {
+		            File file = fc.getSelectedFile();
+		            selectedFile = file.getAbsolutePath();
+		            selectedFileLabel.setText(selectedFile);
+		            selectedFileLabel.updateUI();
+		        }
+		    }
+		});
+		
+		JButton applyButton = new JButton("Read all data");
+		res.add(new JLabel("Data panel, by Tim van Rossum"));
+		res.add(new JLabel("Selected file path:"));
+		res.add(selectedFileLabel);
+		
+		return res;
 	}
 }

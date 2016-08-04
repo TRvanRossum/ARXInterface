eb9344a933257586e342e2a709cc8c02b475e08b
Tim <Tim@145.94.216.25>
2016-08-04 16:50:47 +0200

    Config data holder done.

--- src/view/data/config/Configuration.java
+++ src/view/data/config/Configuration.java
@@ -9,4 +9,16 @@ public class Configuration {
 	private Data data;
 	private Map<String, AttributeType> classification;
 	
+	public Configuration(Data d, Map<String, AttributeType> c) {
+		data = d;
+		classification = c;
+	}
+
+	public Data getData() {
+		return data;
+	}
+
+	public Map<String, AttributeType> getClassification() {
+		return classification;
+	}
 }

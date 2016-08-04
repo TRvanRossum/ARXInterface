25e10b9b5ec4df757c7f8c073a7e027d932c0cb0
Tim <Tim@145.94.216.25>
2016-08-04 13:57:41 +0200

    Getting closer, almost there.

--- src/view/data/attributes/AttributePerColumnPanel.java
+++ src/view/data/attributes/AttributePerColumnPanel.java
@@ -5,6 +5,7 @@ import java.awt.GridLayout;
 import java.util.List;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class AttributePerColumnPanel extends JPanel {
 	
@@ -13,20 +14,26 @@ public class AttributePerColumnPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = 3066477756764410002L;
 	private AttributePerColumnTable table;
+	private JPanel mainPanel;
 
 	public AttributePerColumnPanel(String[] attributes) {
 		table = new AttributePerColumnTable();
+		mainPanel = new JPanel();
 		update(attributes);
 	}
 	
 	public void update(String[] attributes) {
 		removeAll();
+		mainPanel = new JPanel();
 		List<Component> comps = table.getUpdatedGrid(attributes);
 		
-		setLayout(new GridLayout(attributes.length, 2));
+		mainPanel.setLayout(new GridLayout(attributes.length, 2));
 		for(int i = 0; i < comps.size(); i++){
-			add(comps.get(i));
+			mainPanel.add(comps.get(i));
 		}
+		JScrollPane pane = new JScrollPane(mainPanel);
+		add(pane);
 		repaint();
+		System.out.println("Test");
 	}
 }

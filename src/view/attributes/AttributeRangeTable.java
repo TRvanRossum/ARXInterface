2d8678052f5ccb75aa8e7fde47eb2fad99205a79
Tim <Tim@145.94.219.40>
2016-08-09 11:48:03 +0200

    Interim commit.

---
+++ src/view/attributes/AttributeRangeTable.java
@@ -0,0 +1,20 @@
+package view.attributes;
+
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import view.data.config.AttributeType;
+
+public class AttributeRangeTable {
+	public AttributeRangeTable() {
+		
+	}
+	
+	public List<Component> getComponents(Map<String, AttributeType> attToType){
+		List<Component> res = new ArrayList<Component>();
+		// TODO
+		return res;
+	}
+}

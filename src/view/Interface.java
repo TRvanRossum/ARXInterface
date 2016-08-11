6463cce7d6398d4b543b0e81185073bc66a799f9
Tim <Tim@145.94.217.174>
2016-08-11 13:47:19 +0200

    v1.1 push.

--- src/view/Interface.java
+++ src/view/Interface.java
@@ -95,7 +95,7 @@ public class Interface implements ItemListener {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("Interface - Beta v1.0");
+        JFrame frame = new JFrame("Interface - Beta v1.1");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          
         //Create and set up the content pane.

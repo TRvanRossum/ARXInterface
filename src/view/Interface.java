d2422c603436fe6736aa60ead499059ff6ecf880
Tim <Tim@145.94.217.174>
2016-08-11 18:14:52 +0200

    I cannot take it any longer for today, going to leave if I don't get any more productive in the next 15 minutes...

--- src/view/Interface.java
+++ src/view/Interface.java
@@ -102,7 +102,7 @@ public class Interface implements ItemListener {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("Interface - Beta v1.1");
+        JFrame frame = new JFrame("Interface - Beta v1.2");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          
         //Create and set up the content pane.

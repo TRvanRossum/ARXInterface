fcaadbceab19e375352d72af76fe13a219a670b2
Tim <Tim@145.94.218.174>
2016-08-16 13:15:20 +0200

    DGH is now generated, but possibly not that efficient.

--- src/dgh/DGH.java
+++ src/dgh/DGH.java
@@ -1,5 +1,10 @@
 package dgh;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class DGH {
 	private DGHNode startNode;
 	private DGHInput input;
@@ -14,6 +19,28 @@ public class DGH {
 	}
 	
 	public void generate() {
-		startNode.generateNeighbours();
+		List<DGHNode> nextLevel = startNode.generateNeighbours();
+		startNode.setNext(nextLevel);
+		generate(nextLevel);
+	}
+	
+	public void generate(List<DGHNode> curLevel) {
+		if(curLevel.size() == 0) {
+			return;
+		}
+		Set<DGHNode> set = new HashSet<DGHNode>();
+		for(DGHNode node : curLevel) {
+			set.addAll(node.generateNeighbours());
+		}
+		for(DGHNode node : curLevel) {
+			for(DGHNode nextNode : set) {
+				if(node.isValidTransition(nextNode)){
+					node.addNext(nextNode);
+				}
+			}
+		}
+		List<DGHNode> nextLevelNodes = new ArrayList<DGHNode>();
+		nextLevelNodes.addAll(set);
+		generate(nextLevelNodes);
 	}
 }

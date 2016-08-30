7b8ba121fcad87c42aff7ca0f0a99d0043cce6df
Tim <Tim@145.94.217.1>
2016-08-30 18:31:22 +0200

    Low-productivity commit.

--- src/algorithms/KAnonMinGenAlgorithm.java
+++ src/algorithms/KAnonMinGenAlgorithm.java
@@ -4,21 +4,34 @@ import java.util.List;
 
 import dgh.DGH;
 import dgh.DGHInput;
+import dgh.DGHNode;
 import dgh.database.DGHDatabase;
 
 public class KAnonMinGenAlgorithm implements Algorithm {
 	
 	private int k;
 	private DGHInput input;
+	private DGHDatabase db;
 	
 	public KAnonMinGenAlgorithm(int _k, DGHInput _input) {
 		k = _k;
 		input = _input;
+		db = new DGHDatabase(input);
 	}
 	
 	@Override
 	public DGHDatabase apply(DGH dgh) {
-		
+		if(dgh.getStart().getNext() == null) {
+			dgh.generate();
+		}
+		List<DGHNode> nodes = dgh.getStart().getNext();
+		// TODO FUCKING HELL I HAVE THE MOST AMAZING IDEA BUT NO IDEA HOW TO IMPLEMENT IT.
+		return findBestCandidate(null, null);
+	}
+	
+	private DGHDatabase findBestCandidate(List<DGHDatabase> db, List<DGHNode> level) {
+		// TODO FUCKING HELL I HAVE THE MOST AMAZING IDEA BUT NO IDEA HOW TO IMPLEMENT IT.
+		// I have no idea what to code and how to code it...................................
 		return null;
 	}
 	

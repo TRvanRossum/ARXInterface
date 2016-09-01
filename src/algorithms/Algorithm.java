295cbc6543062bc3c6604300f698e57d1e710eb3
Tim <Tim@145.94.217.215>
2016-09-01 16:52:56 +0200

    Having a bad day at work today. Only an hour left though, might go better tomorrow.

--- src/algorithms/Algorithm.java
+++ src/algorithms/Algorithm.java
@@ -4,5 +4,10 @@ import dgh.DGH;
 import dgh.database.DGHDatabase;
 
 public interface Algorithm {
+	/**
+	 * Apply the algorithm on the data, taking into consideration the supplied DGH.
+	 * @param dgh The supplied DGH.
+	 * @return The most accurate database.
+	 */
 	public DGHDatabase apply(DGH dgh);
 }

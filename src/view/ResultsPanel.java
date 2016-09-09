e2b6250871681f74ee375150d3de236cd7bba3a8
Tim <Tim@145.94.218.49>
2016-09-09 11:53:04 +0200

    Thinking about how to visualise the results.

---
+++ src/view/ResultsPanel.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class ResultsPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2767512105398842253L;
+
+}

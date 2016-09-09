789390b4616fbe3325cd228739fde39777270566
Tim <Tim@145.94.218.221>
2016-09-09 15:34:10 +0200

    Made a small demo frame for the results.

--- src/view/ResultsFrame.java
+++ src/view/ResultsFrame.java
@@ -1,6 +1,9 @@
 package view;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import dgh.database.DGHDatabase;
 import utils.WindowUtils;
@@ -18,8 +21,15 @@ public class ResultsFrame extends JFrame {
 		this.setTitle("Results of the anonymization");
 		this.setResizable(true);
 		
-		// TODO code for the menu.
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("Metrics");
+		JMenuItem metricsItem = new JMenuItem("Display metrics");
 		
+		menu.add(metricsItem);
+		menuBar.add(menu);
+		this.setJMenuBar(menuBar);
+		
+		this.pack();
 		
 		WindowUtils.centreWindow(this);
 	}

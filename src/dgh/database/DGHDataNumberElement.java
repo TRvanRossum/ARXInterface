8e8fd34ecf58fcce6f6574502bd9b1e85b162e81
Tim <Tim@145.94.216.141>
2016-09-23 14:37:40 +0200

    Found the issue: attribute string of a map becomes null somehow, need to fix that.

--- src/dgh/database/DGHDataNumberElement.java
+++ src/dgh/database/DGHDataNumberElement.java
@@ -29,7 +29,10 @@ public class DGHDataNumberElement implements DGHDataElement {
 	public void transform(List<? extends Mapping> maps) {
 		for(Mapping m : maps) {
 			if(m instanceof NumericalMapping) {
+				System.out.println(m);
 				NumericalMapping nm = (NumericalMapping) m;
+				System.out.println(nm);
+				System.out.println(nm.getAttributeName());
 				try {
 					if(nm.getAttributeName().equals(attribute) && nm.contains(attribute)) {
 						data = nm.map(attribute, data);

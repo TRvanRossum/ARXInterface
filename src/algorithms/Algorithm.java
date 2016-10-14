cf4155345be4a606402468a8692c1a92d09287c1
trvan <trvan@172.16.20.145>
2016-10-14 12:51:47 +0200

    Added some commentary.

--- src/algorithms/Algorithm.java
+++ src/algorithms/Algorithm.java
@@ -2,7 +2,11 @@ package algorithms;
 
 import dgh.DGH;
 import dgh.database.DGHDatabase;
-
+/**
+ * The Algorithm interface allows us to have a baseline of operations for the algorithm.
+ * @author Tim van Rossum
+ *
+ */
 public interface Algorithm {
 	/**
 	 * Apply the algorithm on the data, taking into consideration the supplied DGH.
--- src/dgh/DGHInput.java
+++ src/dgh/DGHInput.java
@@ -6,12 +6,32 @@ import functions.NumericalMapping;
 import functions.TextualMapping;
 import view.data.config.Configuration;
 
+/**
+ * A data class containing the configuration of data, as well as the defined numerical and
+ * textual mappings.
+ * @author Tim van Rossum
+ *
+ */
 public class DGHInput {
-	
+	/**
+	 * The configuration of data.
+	 */
 	private Configuration config;
+	/**
+	 * The defined textual mappings.
+	 */
 	private List<TextualMapping> textualMapping;
+	/**
+	 * The defined numerical mappings.
+	 */
 	private List<NumericalMapping> numberMapping;
 	
+	/**
+	 * Constructor.
+	 * @param _config The configuration of data.
+	 * @param _textualMapping The defined textual mappings.
+	 * @param _numberMapping The defined numerical mappings.
+	 */
 	public DGHInput(Configuration _config, List<TextualMapping> _textualMapping, List<NumericalMapping> _numberMapping) {
 		config = _config;
 		textualMapping = _textualMapping;

a820b0d87e251e0e3663bdb3add05c0815c39ed5
Tim <trvanrossum@gmail.com>
2016-11-04 15:37:24 +0100

    Working on the algorithm.

--- src/algorithms/LDivAlgorithm.java
+++ src/algorithms/LDivAlgorithm.java
@@ -3,6 +3,11 @@ package algorithms;
 import dgh.DGH;
 import dgh.database.DGHDatabase;
 
-public class LDivAlgorithm implements Algorithm {
-
+public interface LDivAlgorithm extends Algorithm {
+	/**
+	 * Apply the algorithm on the data, taking into consideration the supplied DGH and the already K-anonymized database.
+	 * @param dgh The supplied DGH.
+	 * @return The most accurate database, with anonymized insensitive attributes.
+	 */
+	public DGHDatabase apply(DGHDatabase kAnonDB, DGH dgh); 
 }
---
+++ src/algorithms/LDivStandardAlgorithm.java
@@ -0,0 +1,20 @@
+package algorithms;
+
+import dgh.DGH;
+import dgh.database.DGHDatabase;
+
+public class LDivStandardAlgorithm implements LDivAlgorithm {
+	
+	private int l;
+	
+	public LDivStandardAlgorithm(int _l) {
+		this.l = _l;
+	}
+	
+	@Override
+	public DGHDatabase apply(DGHDatabase kAnonDB, DGH dgh) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
